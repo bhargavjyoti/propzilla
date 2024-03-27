41969c234d45e70609e9c3d459bc37601329bdff
Bhargav Kumar <champion.maverick360@gmail.com>
2024-03-27 16:34:28 +0530

    create user and property models

---
+++ models/Property.js
@@ -0,0 +1,86 @@
+import mongoose from "mongoose"
+
+const PropertySchema = new mongoose.Schema({
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    location: {
+        street: {
+            type: String,
+        },
+        city: {
+            type: String
+        },
+        state: {
+            type: String
+        },
+        zipcode: {
+            type: String
+        }
+    },
+    beds: {
+        type: Number,
+        required: true
+    },
+    baths: {
+        type: Number,
+        required: true
+    },
+    square_feet: {
+        type: Number,
+        required: true
+    },
+    amenities: [
+        {
+            type: String
+        }
+    ],
+    rates: {
+        weekly: {
+            type: Number
+        },
+        monthly: {
+            type: Number
+        },
+        nightly: {
+            type: Number
+        }
+    },
+    seller_info: {
+        name: {
+            type: String
+        },
+        email: {
+            type: String
+        },
+        phone: {
+            type: String
+        }
+    },
+    images: [
+        {
+            type: String
+        }
+    ],
+    is_featured: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true })
+
+const Property = mongoose.model("Property", PropertySchema)
+
+export default Property
---
+++ models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose"
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+    },
+    bookmarks: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Property"
+        }
+    ]
+}, { timestamps: true })
+
+const User = mongoose.model("User", UserSchema)
+
+export default User
