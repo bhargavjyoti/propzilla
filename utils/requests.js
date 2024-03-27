a6de084267cc2d96ff02fe90275479a8e777ecdd
Bhargav Kumar <champion.maverick360@gmail.com>
2024-03-27 23:00:08 +0530

    create get route and fetch all properties

---
+++ utils/requests.js
@@ -0,0 +1,24 @@
+// Get the API domain from the environment variable or use the default value
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null 
+async function fetchProperties() {
+    try {
+        // Handle the case when the API domain is not set
+        if (!apiDomain) {
+            return []
+        }
+
+        // Fetch data from the API
+        const res = await fetch(`${apiDomain}/properties`)
+
+        if (!res.ok) {
+            throw new Error("Failed to fetch data")
+        }
+
+        return res.json()
+    } catch (error) {
+        console.log(error)
+        return []
+    }
+}
+
+export { fetchProperties }
