5630f4676eb7bb7fc7b2c6881458468fc3c4b1cd
Bhargav Kumar <champion.maverick360@gmail.com>
2024-03-28 01:17:56 +0530

    add single property fetching functionality

---
+++ app/properties/[id]/page.jsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { useParams } from "next/navigation"
+import { fetchProperty } from "@/utils/requests"
+
+const PropertyPage = () => {
+  const { id } = useParams()
+
+  const [property, setProperty] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchPropertyData = async () => {
+      if(!id) return
+      try {
+        const property = await fetchProperty(id)
+        setProperty(property)
+      } catch (error) {
+        console.log("Error in fetching property:", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    if(property === null) {
+      fetchPropertyData()
+    }
+  }, [id, property])
+
+  return (
+    <div>
+      <h1>{property?.name}</h1>
+    </div>
+  )
+}
+
+export default PropertyPage
--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,7 @@
 // Get the API domain from the environment variable or use the default value
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null 
+
+// Fetch All Properties
 async function fetchProperties() {
     try {
         // Handle the case when the API domain is not set
@@ -21,4 +23,25 @@ async function fetchProperties() {
     }
 }
 
-export { fetchProperties }
+// Fetch Single Property
+async function fetchProperty(id) {
+    try {
+
+        if(!apiDomain) {
+            return null
+        }
+
+        const res = await fetch(`${apiDomain}/properties/${id}`)
+
+        if(!res.ok) {
+            throw new Error("Failed to fetch data")
+        }
+
+        return res.json()
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
+
+export { fetchProperties, fetchProperty }
