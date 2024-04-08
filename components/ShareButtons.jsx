ac920b35bb28b931a7fe97eb35714606b835c34c
Bhargav Kumar <champion.maverick360@gmail.com>
2024-04-09 00:27:13 +0530

    complete share button functionality

--- components/ShareButtons.jsx
+++ components/ShareButtons.jsx
@@ -1,12 +1,39 @@
-import { FaShare } from "react-icons/fa"
+import { 
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  EmailShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  WhatsappIcon,
+  EmailIcon
+ } from "react-share"
 
-const ShareButtons = () => {
+const ShareButtons = ({ property }) => {
+  const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/property/${property._id}`
+
   return (
-    <button
-        className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
-    >
-        <FaShare className="mr-2"/> Share Property
-    </button>
+    <>
+      <h3 className="text-xl font-bold text-center pt-2">Share This Property: </h3>
+      <div className="flex gap-3 justify-center pb-5">
+        {/* Facebook */}
+        <FacebookShareButton url={shareUrl} quote={property.name} hashtag={`#${property.type}ForRent`}>
+          <FacebookIcon size={40} round={true} />
+        </FacebookShareButton>
+        {/* Twitter */}
+        <TwitterShareButton url={shareUrl} title={property.name} hashtags={[`#${property.type}ForRent`]}>
+          <TwitterIcon size={40} round={true} />
+        </TwitterShareButton>
+        {/* Whatsapp */}
+        <WhatsappShareButton url={shareUrl} title={property.name} separator=":: ">
+          <WhatsappIcon size={40} round={true} />
+        </WhatsappShareButton>
+        {/* Email */}
+        <EmailShareButton subject={property.name} body={`Check out this property on PropZilla:`} url={shareUrl}>
+          <EmailIcon size={40} round={true} />
+        </EmailShareButton>
+      </div>
+    </>
   )
 }
 
