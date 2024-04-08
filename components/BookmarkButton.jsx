0521802079ca8ef43e41a859c4925db321fd3ba4
Bhargav Kumar <champion.maverick360@gmail.com>
2024-04-08 15:39:44 +0530

    complete add bookmark functionality

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,10 +1,48 @@
+"use client"
+import { useState, useEffect } from "react"
+import { useSession } from "next-auth/react"
+import { toast } from "react-toastify"
+
 import { FaBookmark } from "react-icons/fa"
 
-const BookmarkButton = () => {
-  return (
-    <button
-        className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
-    >
+const BookmarkButton = ({ property }) => {
+  const { data: session } = useSession()
+  const userId = session?.user?.id
+
+  const [isBookmarked, setIsBookmarked] = useState(false)
+
+  const handleClick = async () => {
+    if(!userId) {
+      toast.error("Please sign in to bookmark properties")
+      return
+    }
+
+    try {
+      const res = await fetch("/api/bookmarks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ propertyId: property._id })
+      })
+
+      if(res.status === 200) {
+        const data = await res.json()
+        toast.success(data.message)
+        setIsBookmarked(data.isBookmarked)
+      }
+
+    } catch (error) {
+      console.log(error)
+      toast.error("Something went wrong, please try again")
+    }
+  }
+  return isBookmarked ? (
+    <button onClick={handleClick} className="bg-red-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
+        <FaBookmark className="mr-2"/> Remove Bookmark
+    </button>
+  ) : (
+    <button onClick={handleClick} className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
         <FaBookmark className="mr-2"/> Bookmark Property
     </button>
   )
