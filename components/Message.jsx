acc2dd6463e660aec40dafe79a89de7503e9f9e6
Bhargav Kumar <champion.maverick360@gmail.com>
2024-04-13 22:27:08 +0530

    complete delete message functionality

--- components/Message.jsx
+++ components/Message.jsx
@@ -5,6 +5,7 @@ import { toast } from "react-toastify"
 
 const Message = ({ message }) => {
     const [isRead, setIsRead] = useState(message.read)
+    const [isDeleted, setIsDeleted] = useState(false)
 
     const handleReadClick = async () => {
         try {
@@ -27,47 +28,67 @@ const Message = ({ message }) => {
         }
     }
 
-  return (
-    <div className="relative bg-white p-4 rounded-md shadow-md border border-gray-200">
-        {!isRead && (
-            <div className="absolute top-2 right-2 bg-yellow-500 text-white px-2 py-1 rounded-md">New</div>
-        )}
-        <h2 className="text-xl mb-4">
-            <span className="font-bold">Property Inquiry: </span>
-            {message.property.name}
-        </h2>
-        <p className="text-gray-700">
-           {message.body}
-        </p>
+    const handleDeleteClick = async () => {
+        try {
+          const res = await fetch(`/api/messages/${message._id}`, {
+            method: 'DELETE',
+          });
+    
+          if (res.status === 200) {
+            setIsDeleted(true);
+            toast.success('Message Deleted');
+          }
+        } catch (error) {
+          console.log(error);
+          toast.error('Message was not deleted');
+        }
+      };
+    
+      if (isDeleted) {
+        return null;
+      }
 
-        <ul className="mt-4">
-            <li><strong>Name: </strong>{message.sender.username}</li>
+    return (
+        <div className="relative bg-white p-4 rounded-md shadow-md border border-gray-200">
+            {!isRead && (
+                <div className="absolute top-2 right-2 bg-yellow-500 text-white px-2 py-1 rounded-md">New</div>
+            )}
+            <h2 className="text-xl mb-4">
+                <span className="font-bold">Property Inquiry: </span>
+                {message.property.name}
+            </h2>
+            <p className="text-gray-700">
+            {message.body}
+            </p>
 
-            <li>
-            <strong>Reply Email: </strong>
-            <a href={`mailto:${message.email}`} className="text-blue-500"
-                >{message.email}</a
+            <ul className="mt-4">
+                <li><strong>Name: </strong>{message.sender.username}</li>
+
+                <li>
+                <strong>Reply Email: </strong>
+                <a href={`mailto:${message.email}`} className="text-blue-500"
+                    >{message.email}</a
+                >
+                </li>
+                <li>
+                <strong>Reply Phone: </strong>
+                <a href={`tel:${message.phone}`} className="text-blue-500"
+                    >{message.phone}</a
+                >
+                </li>
+                <li><strong>Received: </strong>{new Date(message.createdAt).toLocaleString()}</li>
+            </ul>
+            <button
+                onClick={handleReadClick}
+                className={`mt-4 mr-3 ${isRead ? "bg-gray-300" : "bg-blue-500 text-white"} py-1 px-3 rounded-md`}
             >
-            </li>
-            <li>
-            <strong>Reply Phone: </strong>
-            <a href={`tel:${message.phone}`} className="text-blue-500"
-                >{message.phone}</a
-            >
-            </li>
-            <li><strong>Received: </strong>{new Date(message.createdAt).toLocaleString()}</li>
-        </ul>
-        <button
-            onClick={handleReadClick}
-            className={`mt-4 mr-3 ${isRead ? "bg-gray-300" : "bg-blue-500 text-white"} py-1 px-3 rounded-md`}
-        >
-            {isRead ? "Mark As New" : "Mark As Read"}
-        </button>
-        <button className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">
-            Delete 
-        </button>
-    </div>
-  )
-}
+                {isRead ? "Mark As New" : "Mark As Read"}
+            </button>
+            <button onClick={handleDeleteClick} className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">
+                Delete 
+            </button>
+        </div>
+    )
+    }
 
 export default Message
