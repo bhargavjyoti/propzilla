4b121892fbb53422c17eabbe0a9868cbbafc90c7
Bhargav Kumar <champion.maverick360@gmail.com>
2024-04-13 19:10:28 +0530

    fix bug

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -17,7 +17,7 @@ export const GET = async () => {
 
         const { userId } = sessionUser
 
-        const messages = await Message.find({ recipient: userId }).populate("sender", "name").populate("property", "title")
+        const messages = await Message.find({ recipient: userId }).populate("sender", "username").populate("property", "name")
 
         return new Response(JSON.stringify(messages), { status: 200 })
         
