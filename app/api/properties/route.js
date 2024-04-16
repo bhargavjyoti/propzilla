fce0235a467e39d9b09b3b0b399d678a738e5143
Bhargav Kumar <champion.maverick360@gmail.com>
2024-04-16 14:07:21 +0530

    add pagination api route

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,39 +1,53 @@
-import connectDB from "@/config/database"
-import Property from "@/models/Property"
-import { getSessionUser } from "@/utils/getSessionUser"
-import cloudinary from "@/config/cloudinary"
-
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import cloudinary from "@/config/cloudinary";
 
 // GET /api/properties
 export const GET = async (request) => {
     try {
-        await connectDB()
-        const properties = await Property.find({})
-        return new Response(JSON.stringify(properties), { status: 200 })
+        await connectDB();
+
+        const page = request.nextUrl.searchParams.get("page") || 1;
+        const pageSize = request.nextUrl.searchParams.get("pageSize") || 9;
+
+        const skip = (page - 1) * pageSize;
+
+        const total = await Property.countDocuments({});
+        const properties = await Property.find({}).skip(skip).limit(pageSize);
+
+        const result = {
+            total,
+            properties,
+        };
+
+        return new Response(JSON.stringify(result), { status: 200 });
     } catch (error) {
-        console.log(error)
-        return new Response("Something went wrong", { status: 500 })
+        console.log(error);
+        return new Response("Something went wrong", { status: 500 });
     }
-}
+};
 
 // POST /api/properties
 export const POST = async (request) => {
     try {
-        await connectDB()
+        await connectDB();
 
-        const sessionUser = await getSessionUser()
+        const sessionUser = await getSessionUser();
 
         if (!sessionUser || !sessionUser.userId) {
-            return new Response("UserId is required", { status: 401 })
+            return new Response("UserId is required", { status: 401 });
         }
-        
-        const { userId } = sessionUser
 
-        const formData = await request.formData()
+        const { userId } = sessionUser;
+
+        const formData = await request.formData();
 
         // Access all values from amenities and images
-        const amenities = formData.getAll('amenities')
-        const images = formData.getAll('images').filter((image) => image.name !== "")
+        const amenities = formData.getAll("amenities");
+        const images = formData
+            .getAll("images")
+            .filter((image) => image.name !== "");
 
         // Create propertyData object for database
         const propertyData = {
@@ -60,42 +74,45 @@ export const POST = async (request) => {
                 email: formData.get("seller_info.email"),
                 phone: formData.get("seller_info.phone"),
             },
-            owner: userId
-        }
+            owner: userId,
+        };
 
         // Upload images to cloudinary
-        const imageUploadPromises = []
+        const imageUploadPromises = [];
 
-        for(const image of images) {
-            const imageBuffer = await image.arrayBuffer()
-            const imageArray = Array.from(new Uint8Array(imageBuffer))
-            const imageData = Buffer.from(imageArray)
+        for (const image of images) {
+            const imageBuffer = await image.arrayBuffer();
+            const imageArray = Array.from(new Uint8Array(imageBuffer));
+            const imageData = Buffer.from(imageArray);
 
             // Convert image data to base64
-            const imageBase64 = imageData.toString("base64")
+            const imageBase64 = imageData.toString("base64");
 
             // Make request to cloudinary
             const result = await cloudinary.uploader.upload(
-                `data:image/png;base64,${imageBase64}`, {
-                    folder: "PropZilla"
+                `data:image/png;base64,${imageBase64}`,
+                {
+                    folder: "PropZilla",
                 }
-            )
+            );
 
-            imageUploadPromises.push(result.secure_url)
+            imageUploadPromises.push(result.secure_url);
 
             // Wait for all images to upload
-            const uploadedImages = await Promise.all(imageUploadPromises)
+            const uploadedImages = await Promise.all(imageUploadPromises);
             // Add uploaded images to propertyData object
-            propertyData.images = uploadedImages
+            propertyData.images = uploadedImages;
         }
 
-        const newProperty = new Property(propertyData)
-        await newProperty.save()
+        const newProperty = new Property(propertyData);
+        await newProperty.save();
 
-        return Response.redirect(`${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`)
+        return Response.redirect(
+            `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`
+        );
 
         // return new Response(JSON.stringify({message: "Success"}), { status: 200 })
     } catch (error) {
-        return new Response("Failed to add property", { status: 500 })
+        return new Response("Failed to add property", { status: 500 });
     }
-}
+};
