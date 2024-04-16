fa03fecd9700489a9a4f8d22e2afb0403cb7a34f
Bhargav Kumar <champion.maverick360@gmail.com>
2024-04-16 15:21:44 +0530

    add lightbox to images

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,32 +1,33 @@
-import "@/assets/styles/globals.css"
-import AuthProvider from "@/components/AuthProvider"
-import Footer from "@/components/Footer"
-import Navbar from "@/components/Navbar"
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "@/assets/styles/globals.css";
+import AuthProvider from "@/components/AuthProvider";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { GlobalProvider } from "@/context/GlobalContext";
+import "photoswipe/dist/photoswipe.css";
 
 export const metadata = {
     title: "PropZilla",
     description: "A Real Estate Website Built with Next.js",
     keywords: "Real Estate, Property, Web Development, Next.js",
-}
+};
 
 const MainLayout = ({ children }) => {
-  return (
-    <GlobalProvider>
-      <AuthProvider>
-        <html lang="en">
-            <body>
-                <Navbar />
-                <main>{children}</main>
-                <Footer />
-                <ToastContainer />
-            </body>
-        </html>
-      </AuthProvider>
-    </GlobalProvider>
-  )
-}
+    return (
+        <GlobalProvider>
+            <AuthProvider>
+                <html lang="en">
+                    <body>
+                        <Navbar />
+                        <main>{children}</main>
+                        <Footer />
+                        <ToastContainer />
+                    </body>
+                </html>
+            </AuthProvider>
+        </GlobalProvider>
+    );
+};
 
-export default MainLayout
+export default MainLayout;
